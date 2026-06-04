2b72ccd12d35b7635e3fd4ea50a37808d0d38f8c
bachtiar <bachtiarriza87@gmail.com>
2026-06-04 09:43:39 +0700

    feat(role): get, update, & delete role

--- src/modules/roles/services/role.service.ts
+++ src/modules/roles/services/role.service.ts
@@ -3,7 +3,13 @@ import { IRole, Role } from '../entities/role.entity';
 import { RoleRepository } from '../repositories/role.repository';
 import { EntityManager } from '@mikro-orm/postgresql';
 import { CreateRoleDto } from '../dtos/create-role.dto';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { FilterRoleDto } from '../dtos/filter-role.dto';
+import {
+  paginate,
+  PaginatedResult,
+} from '../../../common/utils/pagination.util';
+import { UpdateRoleDto } from '../dtos/update-role.dto';
 
 export class RoleService {
   constructor(
@@ -23,5 +29,42 @@ export class RoleService {
     const role = this.roleRepository.create(createRoleDto);
     await this.em.persist(role).flush();
     return role;
+  }
+
+  async findAll(filterDto: FilterRoleDto): Promise<PaginatedResult<IRole>> {
+    const { ...paginationQuery } = filterDto;
+
+    return paginate<IRole>(this.roleRepository, paginationQuery, {
+      searchFields: ['name'],
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+
+  async findById(id: string): Promise<IRole> {
+    const role = await this.roleRepository.findOne(id);
+    if (!role) throw new NotFoundException('Role not found');
+    return role;
+  }
+
+  async update(id: string, updateRoleDto: UpdateRoleDto): Promise<IRole> {
+    const role = await this.findById(id);
+
+    if (updateRoleDto && updateRoleDto.name !== role.name) {
+      const existRole = await this.roleRepository.findOne({
+        name: updateRoleDto.name,
+      });
+      if (existRole) {
+        throw new ConflictException('Role with this name already exist');
+      }
+    }
+
+    this.em.assign(role, updateRoleDto);
+    await this.em.flush();
+    return role;
+  }
+
+  async delete(id: string): Promise<void> {
+    const role = await this.findById(id);
+    await this.em.remove(role).flush();
   }
 }
