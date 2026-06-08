4684e5ca7e4f6ba588967a4f5f444cc2d299bf72
bachtiar <bachtiarriza87@gmail.com>
2026-06-08 13:29:34 +0700

    feat(user): get all user and by id

---
+++ src/modules/users/entities/filter-user.dto.ts
@@ -0,0 +1,10 @@
+import { Transform } from 'class-transformer';
+import { PaginationQueryDto } from '../../../common/dtos/pagination.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
+
+export class FilterUserDto extends PaginationQueryDto {
+  @Transform(({ value }) => value === 'true' || value == true)
+  @IsBoolean()
+  @IsOptional()
+  isActive?: boolean;
+}
